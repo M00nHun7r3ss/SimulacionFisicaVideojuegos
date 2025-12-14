414b05faa192f6d2096c632a99de79bd0dc3cfbb
M00nHun7r3ss <116196853+M00nHun7r3ss@users.noreply.github.com>
2025-12-14 12:27:06 +0100

    Update Scene.cpp

--- skeleton/Scene.cpp
+++ skeleton/Scene.cpp
@@ -19,6 +19,7 @@ void Scene::exitScene()
 // Scene0, Scene1, Scene2 ... 
 
 // ---- SCENE 0 ----
+Scene0::Scene0(PxMaterial* material) : gMaterial(material), _fixedPos(PxVec3(0, 50, 0)), _kFixed(100), _restLengthFixed(5), _k(100), _restLength(5)
 {
 }
 
@@ -39,6 +40,7 @@ void Scene0::init()
 	//La registramos
 	RegisterRenderItem(_fixedObject);
 	_fixedObject->color = Vector4(0.2, 0.2, 0.5, 1.0);
+
 
 	//// ---------- PARTICULAS ----------
 	//a = new Particle(PxVec3(0, 40, 0), PxVec3(0, 0, 0), PxVec4(1, 0, 0, 1), PxVec3(0, 0, 0), 10, 0.1, 100, 1);
@@ -122,19 +124,88 @@ void Scene0::init()
 	_particleSystem->addForceGenerator(_gravityForce);
 
 	// - EJERCICIO OPCIONAL SLINKY -
+
+	// ---------- PARTICULAS ----------
+	//a = new Particle(PxVec3(0, 40, 0), PxVec3(0, 0, 0), PxVec4(1, 0, 0, 1), PxVec3(0, 0, 0), 1, 0.1, 100, 1);
+	//b = new Particle(PxVec3(10, 40, 0), PxVec3(0, 0, 0), PxVec4(0, 1, 0, 1), PxVec3(0, 0, 0), 1, 0.1, 100, 1);
+
+	// - EJERCICIO OPCIONAL SLINKY -
+	int count = 5;
+	float separation = 3.0f;
+
+	_particleSystem = new ParticleSystem();
+
+	//Creamos las partivulas
+	for (int i = 0; i < count; i++) {
+		Particle* p = new Particle(
+			PxVec3(0, 50 - i * separation, 0),  
+			PxVec3(0, 0, 0),                    
+			PxVec4(1, 1, 0, 1),                 
+			PxVec3(0, 0, 0),             
+			1.0f,                            
+			0.1f,                           
+			100.0f,                             
+			1.0f);                          
+		_slinky.push_back(p);
+		_particleSystem->addParticle(p);
+	}
+
+	_particleSystem->setPaused(false);
+
+	//Creamos los muelles
+	float k = 80.0f;    
+	float restLength = separation; 
+
+	for (int i = 0; i < count - 1; i++) {
+		SpringForceGenerator* spring = new SpringForceGenerator(
+			_slinky[i],
+			_slinky[i + 1],
+			k,
+			restLength
+		);
+		_particleSystem->addForceGenerator(spring);
+	}
+
+	//Gravedad
+	_gravityForce = new GravityForce(PxVec3(0, -9.8, 0));
+	_particleSystem->addForceGenerator(_gravityForce);
+
+
+	////Para que funcione con sistemas de particulas
+	//_particleSystem = new ParticleSystem();
+	//_particleSystem->addParticle(a);
+	//_particleSystem->addParticle(b);
+	//_particleSystem->setPaused(false);
+
+	//_aGenerator = new UniformParticleGenerator(PxVec3(0, 40, 0), 1,
+	//	PxVec3(0, 40, 0), PxVec3(0, 40, 0),
+	//PxVec3(0.0, -10.0, 0.0), PxVec3(0.0, 10.0, 0.0));
+	//_aGenerator->setProbability(1);
+	//_particleSystem->addGenerator(_aGenerator);
+
+	//_bGenerator = new UniformParticleGenerator(PxVec3(10, 40, 0), 1,
+	//	PxVec3(10, 40, 0), PxVec3(10, 40, 0),
+	//	PxVec3(0.0, -10.0, 0.0), PxVec3(0.0, 10.0, 0.0));
+	//_bGenerator->setProbability(1);
+	//_particleSystem->addGenerator(_bGenerator);
+
+	//// ---------- MUELLE FIJO - PARTICULA ----------
+	//_FixedSpringForce = new FixedSpringForceGenerator(_fixedPos, _kFixed, _restLengthFixed);
+	//_particleSystem->addForceGenerator(_FixedSpringForce);
+
+	//// ---------- MUELLE PARTICULA - PARTICULA ----------
+	//_SpringForce = new SpringForceGenerator(a, b, 50, 5);
+	//_particleSystem->addForceGenerator(_SpringForce);
+
+	//// ---------- GRAVEDAD ----------
+	//_gravityForce = new GravityForce(PxVec3(0, -9.8, 0));
+	//_particleSystem->addForceGenerator(_gravityForce);
+
 }
 
 void Scene0::update(double t)
 {
 	_particleSystem->update(t);
-
-	if (_windActive) {
-		_windTimer -= t;
-		if (_windTimer <= 0) {
-			_particleSystem->removeForceGenerator(_lateralForce);
-			_windActive = false;
-		}
-	}
 }
 
 void Scene0::cleanup()
@@ -158,19 +229,17 @@ void Scene0::handleKey(unsigned char key, const PxTransform& camera)
 	case 'Q':
 		_kFixed = max(0, _kFixed - 1);
 		_FixedSpringForce->setK(_kFixed);
+		std::cout << _kFixed;
 		break;
 		//K aumenta
 	case 'W':
 		_kFixed += 1;
 		_FixedSpringForce->setK(_kFixed);
+		std::cout << _kFixed;
 		break;
 		//Aniade una fuerza
 	case 'E':
-		if (!_windActive) {
-			_particleSystem->addForceGenerator(_lateralForce);
-			_windActive = true;
-			_windTimer = 2.0;
-		}
+		a->addForce(PxVec3(200, 0, 0));
 		break;
 
 	default:
